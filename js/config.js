27372024f6ac59559c0cfac1568c17c466d1341b
shadyonfire <yaiamshailesh@gmail.com>
2017-09-20 10:29:08 +0530

    added routing and module to single page

--- js/config.js
+++ js/config.js
@@ -1,3 +1,50 @@
 /**
  * Created by shady on २०-०९-२०१७.
  */
+
+app.config(config);
+config.$inject["$routeProvider"];
+function config($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl : "modules/main.php",
+        controller:"main"
+        })
+        .when("/balance", {
+            templateUrl : "modules/balance.php",
+            controller:"balance"
+             })
+        .when("/history", {
+            templateUrl : "modules/history.php",
+            controller:"history"
+
+        })
+        .when("/referrals", {
+            templateUrl : "modules/referrals.php",
+            controller: "referrals"
+        })
+        .when("/sendmoney",{
+            templateUrl:"modules/sendmoney.php",
+            controller:"sendmoney"
+        })
+        .when("/requestmoney",{
+            templateUrl:"modules/requestmoney.php",
+            controller:"recievemoney"
+        })
+        .when("/freeze",{
+            templateUrl:"modules/freeze.php",
+            controller:"freeze"
+        })
+        .when("/profile",{
+            templateUrl:"modules/profile.php",
+            controller:"profile"
+        })
+        .when("/support",{
+            templateUrl:"modules/support.php",
+            controller:"support"
+        })
+        .otherwise(
+            {
+                templateUrl:"modules/main.php"
+            }
+        );
+};
--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,6 @@
 /**
  * Created by shady on २०-०९-२०१७.
  */
+app.controller("ctrl",function($scope){
+    $scope.x="hello";
+})
